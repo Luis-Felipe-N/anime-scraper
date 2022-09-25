db4c7302a871fa762014ef46076330ef03f0c12e
luis.nunes <luis.nunes@mail.uft.edu.br>
2022-09-25 11:50:30 -0300

    :bug: fix : cors eror

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata"
+import cors from 'cors'
 import express from 'express'
 import { router } from './routes'
 import { createConnection } from "./database/dataSource"
-
 const app = express()
 
 createConnection()
 
 app.use(express.json())
 
+app.use(cors())
+
 app.use(router)
 
-app.listen(3333, () => {console.log('Server is running in http://localhost:3333')})
+app.listen(3333, () => {console.log('Server is running')})
