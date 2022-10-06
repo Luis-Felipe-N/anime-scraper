343224097bae895d315a85584ca472cbd3b9d7a0
luis.nunes <luis.nunes@mail.uft.edu.br>
2022-10-05 22:41:48 -0300

    :bug: fix : error linkEmbed undefined

--- src/routes/animes.routes.ts
+++ src/routes/animes.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ListAnimesController } from "../module/Anime/ListAnime/ListAnimesController";
 import { ListAnimeBySlugController } from "../module/Anime/ListAnimeBySlug/ListAnimeBySlugController";
+import { ListAnimePopularController } from "../module/Anime/ListAnimePopular/ListAnimePopularController";
 import { ListEpisodeController } from "../module/Anime/ListEpisode/ListEpisodeController";
 import { ListEpisodesByController } from "../module/Anime/ListEpisodesBySeason/ListEpisodesByController";
 import { ListSeasonByIdController } from "../module/Anime/ListSeason/ListSeasonByIdController";
@@ -10,6 +11,7 @@ import { UpdateAnimesByGenreController } from "../module/Anime/UpdateAnimeByGenr
 const animeRouter = Router()
 
 animeRouter.get('/', new ListAnimesController().handle) // OK
+animeRouter.get('/popular', new ListAnimePopularController().handle) // OK
 animeRouter.get('/:slug', new ListAnimeBySlugController().handle) // OK
 animeRouter.get('/:slug/seasons/', new ListSeasonsByAnimeController().handle) // OK
 animeRouter.get('/season/:seasonId', new ListSeasonByIdController().handle) // OK
